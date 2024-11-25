13cdb00289c5534a287883c95f19b30a699379d9
emrekibar93 <emrekibar93@gmail.com>
2024-11-26 00:33:27 +0300

    File upload added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs')
 const path = require('path')
 const Photo = require('./models/Photo')
@@ -9,17 +10,18 @@ const app = express();
 mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //TEPMLATE ENGINE 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
 
 //MIDDLEWARE
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload())
 
 app.get('/', async (req, res) => {
-    const photos = await Photo.find({})
-    res.render('index',{
+    const photos = await Photo.find({}).sort('-dateCreated')
+    res.render('index', {
         photos
     })
 })
@@ -30,16 +32,32 @@ app.get('/about', (req, res) => {
 
 app.get('/photos/:id', async (req, res) => {
     const photo = await Photo.findById(req.params.id)
-     res.render('photo',{photo})
+    res.render('photo', { photo })
 })
 
 app.get('/add', (req, res) => {
     res.render('add')
 })
 
-app.post('/photos', async(req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')
+app.post('/photos', async (req, res) => {
+
+    //console.log(req.files.image)
+
+    //await Photo.create(req.body)
+    
+
+    let uploadImage = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadImage.name
+    uploadImage.mv(uploadPath,    async() => {
+        await Photo.create({
+            ...req.body, 
+            image:'/uploads/' + uploadImage.name
+        })
+        res.redirect('/')
+    })
+
+
+
 })
 
 
