80c1f46cdcc7cca573f4a8149f019249f9e24d44
emrekibar93 <emrekibar93@gmail.com>
2024-11-25 18:18:03 +0300

    Model ve Dinamik icerik eklendi

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const ejs = require('ejs')
 const path = require('path')
-
+const Photo = require('./models/Photo')
 const app = express();
+
+// connect db
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //TEPMLATE ENGINE 
 app.set("view engine","ejs");
@@ -13,8 +17,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({})
+    res.render('index',{
+        photos
+    })
 })
 
 app.get('/about', (req, res) => {
@@ -24,8 +31,8 @@ app.get('/add', (req, res) => {
     res.render('add')
 })
 
-app.post('/photos', (req, res) => {
-    console.log(req.body)
+app.post('/photos', async(req, res) => {
+    await Photo.create(req.body)
     res.redirect('/')
 })
 
