97438b402a02c1848cdec781f3b5c5f478684f2c
emrekibar93 <emrekibar93@gmail.com>
2024-11-26 23:34:59 +0300

    port addres update

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/photos/edit/:id', pageController.getEditPage)
 
 
 
-const port = 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda baslatildi`)
