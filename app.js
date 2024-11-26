7fe3caca6a7fbaae858041d356f48b257e11646a
emrekibar93 <emrekibar93@gmail.com>
2024-11-26 16:05:41 +0300

    Delete Photo

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const fs = require('fs')
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override')
 const ejs = require('ejs')
 const path = require('path')
 const Photo = require('./models/Photo')
+
 const app = express();
 
 // connect db
@@ -18,6 +21,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(fileUpload())
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }))
 
 app.get('/', async (req, res) => {
     const photos = await Photo.find({}).sort('-dateCreated')
@@ -39,19 +43,27 @@ app.get('/add', (req, res) => {
     res.render('add')
 })
 
+
+app.get('/photos/edit/:id', async (req, res) => {
+    const photo = await Photo.findOne({
+        _id: req.params.id
+    })
+    res.render('edit', { photo })
+})
+
 app.post('/photos', async (req, res) => {
 
     //console.log(req.files.image)
 
     //await Photo.create(req.body)
-    
+
 
     let uploadImage = req.files.image;
     let uploadPath = __dirname + '/public/uploads/' + uploadImage.name
-    uploadImage.mv(uploadPath,    async() => {
+    uploadImage.mv(uploadPath, async () => {
         await Photo.create({
-            ...req.body, 
-            image:'/uploads/' + uploadImage.name
+            ...req.body,
+            image: '/uploads/' + uploadImage.name
         })
         res.redirect('/')
     })
@@ -60,6 +72,25 @@ app.post('/photos', async (req, res) => {
 
 })
 
+app.put('/photos/:id', async (req, res) => {
+    const photo = await Photo.findOne({
+        _id: req.params.id
+    })
+    photo.title = req.body.title;
+    photo.description = req.body.description;
+    photo.save()
+    res.redirect(`/photos/${req.params.id}`)
+})
+
+app.delete('/photos/:id', async (req, res) => {
+
+    const photo = await Photo.findOne({ _id: req.params.id })
+    let deletedImage = __dirname + '/public' + photo.image
+    fs.unlinkSync(deletedImage);
+    await Photo.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+
+})
 
 
 const port = 3000;
